48ea1ab1ff615e934380d73a9aaa254719e32115
채지민 <chaejimin2002@naver.com>
2024-01-17 20:17:47 +0900

    chae

--- src/administer/Potal.java
+++ src/administer/Potal.java
@@ -9,22 +9,32 @@ public class Potal {
 	public Potal() {}
 	
 	
-	private static int serialNumber = 202310000;
-	private static int serialNumberInitial = 202310001;
-	private int studentID;
-	public String studentName;
-	private int grade;
-	private String address;
-	ArrayList<Subject> subjectList;
-	
+	private int serialNumber = 202310000;
+	private final int serialNumberInitial = 202310001;
+
+	private int serialSubject = 0;
+	private final int serialSubjectInitial = 1;
 	
 	public void studentAdd(String studentName){
 		serialNumber++;
-		totalStudentList.add(new Student(serialNumber));
+		totalStudentList.add(new Student(serialNumber, studentName));
 		
+		System.out.println("추가됨");
+	}
 	
-		totalStudentList.get(serialNumber - serialNumberInitial).studentName = studentName;
+	public void subjectAdd(String subjectName, String professorName) {
+		serialSubject ++;
+		totalSubjectList.add(new Subject(serialSubject, subjectName, professorName));
 		
+		System.out.println("추가됨");
+	}
+	
+	public void showStudentInfo(Student studentName) {
+		System.out.println("==== 학생 정보 출력 ====");
+		System.out.println("학생이름 : " + studentName.studentName);
+		System.out.println("학번 : " + studentName.getStudentID());
+		System.out.println("수강 내역");
+		for ()
 		
 	}
 
