a74c622f6a2aa67ab8602a8c5db52558e6ecb2f4
이상우 <rain030411@snu.ac.kr>
2024-01-23 20:24:23 +0900

    aaaaa

--- src/administer/Main.java
+++ src/administer/Main.java
@@ -1,7 +1,32 @@
 package administer;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		boolean program = true;
+		Scanner sc = new Scanner(System.in);
+		
+		String initial = "Student management integrated system"
+				+ "\nmade by LEE SANG WOO. CHAE JI MIN."
+				+ "\n\n============================="
+				+ "\n1 program exit"
+				+ "\n2 add student"
+				+ "\n3 add subject"
+				+ "\n";
+		
+		while (program) {
+			System.out.println(initial);
+			int command = sc.nextInt();
+			
+			switch (command) {
+			case 1:
+				program = false;
+			case 2:
+				//
+			case 3:
+				//
+			}
+		}
 	}
 }
